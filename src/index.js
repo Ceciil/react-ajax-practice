f82e69be45a7a071deb8afb24c9ac2388d5de258
Angela Lee <rubii.kun@gmail.com>
2019-01-21 21:39:03 -0800

    Import modules and render DOM

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//import App.jsx
+import App from './components/App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//render App.jsx
+ReactDOM.render(<App />, document.getElementById('app'));
